fa77d061e40f910923aa7f32f6c17b71b442cf8d
nottiffchan <nottiffchan@gmail.com>
2021-01-07 22:43:52 +0800

    Add routing paths to name input and alice vibe pages

---
+++ src/app/alice-vibe-page/alice-vibe-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alice-vibe-page',
+  templateUrl: './alice-vibe-page.component.html',
+  styleUrls: ['./alice-vibe-page.component.css']
+})
+export class AliceVibePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
